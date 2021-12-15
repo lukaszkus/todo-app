d610afd44e5551e2db589d63ad7c5f702a84110a
lukaszkus <kusiu@vp.pl>
2021-12-15 12:48:01 +0100

    rearranged files structure

--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -1,15 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }  
-
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     transition: all 0.5s linear;
+    background-image: ${({ theme }) => theme.bgMobImg};
+    background-repeat: no-repeat;
+    background-position: top center;
+  }
+  @media screen and (min-width: 768px){
+    body {
+      background-image: ${({ theme }) => theme.bgDeskImg};
+    }
   }
 `;
--- src/components/themes.js
+++ src/components/themes.js
@@ -1,11 +1,20 @@
+import imgMobLight from "../images/bg-mobile-light.jpg";
+import imgMobDark from "../images/bg-mobile-dark.jpg";
+import imgDeskLight from "../images/bg-desktop-light.jpg";
+import imgDeskDark from "../images/bg-desktop-dark.jpg";
+
 export const lightTheme = {
-  body: "#fff",
-  text: "#000",
-  background: "#fff",
+  body: "var(--clr-l-very-light-G)",
+  text: "var(--clr-d-very-dark-B)",
+  background: "var(--clr-l-very-light-G)",
+  bgMobImg: `url(${imgMobLight})`,
+  bgDeskImg: `url(${imgDeskLight})`,
 };
 
 export const darkTheme = {
-  body: "#000",
-  text: "#fff",
-  background: "#000",
+  body: "var(--clr-d-very-dark-B)",
+  text: "var(--clr-l-very-light-G)",
+  background: "var(--clr-d-very-dark-B)",
+  bgMobImg: `url(${imgMobDark})`,
+  bgDeskImg: `url(${imgDeskDark})`,
 };
