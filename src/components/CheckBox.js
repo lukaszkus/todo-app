3f440ade716a16c331d2ebd10ef7e807fa66a496
lukaszkus <kusiu@vp.pl>
2022-01-25 13:44:22 +0100

    fixed checkbox component

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -2,70 +2,60 @@ import React from "react";
 import styled from "styled-components";
 
 import { clr } from "../utils/variables";
-// import checkIcon from "../../images/icon-check.svg";
+import checkIcon from "../images/icon-check.svg";
 
-const CheckboxContainer = styled.div`
-  display: inline-block;
-  vertical-align: middle;
+const CheckboxContainer = styled.div``;
+
+const CheckboxInput = styled.input`
+  display: none;
 `;
 
-const Icon = styled.svg`
-  fill: none;
-  stroke: white;
-  stroke-width: 2px;
-`;
-
-// Hide checkbox visually but remain accessible to screen readers.
-// Source: https://polished.js.org/docs/#hidevisually
-const HiddenCheckbox = styled.input.attrs({ type: "checkbox" })`
-  border: 0;
-  clip: rect(0 0 0 0);
-  clippath: inset(50%);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-`;
-
-const StyledCheckbox = styled.div`
-  display: inline-block;
+const CheckboxLabel = styled.label`
+  display: grid;
+  place-content: center;
+  position: relative;
+  cursor: pointer;
   width: 18px;
   height: 18px;
-  background: ${(props) => (props.checked ? clr.gradient : "transparent")};
-  border: 1px solid ${({ theme }) => theme.line};
   border-radius: 50%;
-  transition: all 150ms;
-  //   width: 18px;
-  //   height: 18px;
-  //   border: 1px solid ${({ theme }) => theme.line};
-  //   border-radius: 50%;
-  //   transition: all 150ms;
+  border: 1px solid ${({ theme }) => theme.line};
 
   &:hover {
     border: 1px solid ${clr.bright};
-    cursor: pointer;
   }
 
-  ${HiddenCheckbox}:focus + & {
-    box-shadow: 0 0 0 3px ${({ theme }) => theme.text}};
+  &:before {
+    content: "";
+    width: 17px;
+    height: 17px;
+    border-radius: 50%;
+    background: ${(props) => (props.checked ? clr.gradient : "transparent")};
   }
 
-  ${Icon} {
-    visibility: ${(props) => (props.checked ? "visible" : "hidden")};
+  &:after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 11px;
+    height: 9px;
+    background: url(${(props) => (props.checked ? checkIcon : null)});
   }
 `;
 
-const Checkbox = ({ checked, ...props }) => (
+const Checkbox = ({ id, checked, setChecked, handleUpdate }) => (
   <CheckboxContainer>
-    <HiddenCheckbox checked={checked} {...props} />
-    <StyledCheckbox checked={checked}>
-      <Icon viewBox="0 0 24 24">
-        <polyline points="20 6 9 17 4 12" />
-      </Icon>
-    </StyledCheckbox>
+    <CheckboxInput
+      id={`checkbox-${id}`}
+      type="checkbox"
+      checked={checked}
+      onChange={handleUpdate}
+    />
+    <CheckboxLabel
+      htmlFor={`checkbox-${id}`}
+      checked={checked}
+      onClick={() => setChecked(!checked)}></CheckboxLabel>
   </CheckboxContainer>
 );
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,11 +5,11 @@ import { db } from "../utils/firebaseConfig";
 
 import styled from "styled-components";
 // import { clr } from "../utils/variables";
-// import checkIcon from "../../images/icon-check.svg";
+// import checkIcon from "../images/icon-check.svg";
 
 import Button from "./Button";
 import EditTodo from "./EditTodo";
-import CheckBox from "./CheckBox";
+import Checkbox from "./CheckBox";
 
 const TodoItem = styled.li`
   display: flex;
@@ -22,72 +22,22 @@ const TodoItem = styled.li`
   }
 `;
 
-// const CheckBoxContainer = styled.div`
-//   display: inline-block;
-//   vertical-align: middle;
-// `;
-
-// const HiddenCheckBox = styled.input.attrs({ type: "checkbox" })`
-//   border: 0;
-//   clip: rect(0 0 0 0);
-//   clippath: inset(50%);
-//   height: 1px;
-//   margin: -1px;
-//   overflow: hidden;
-//   padding: 0;
-//   position: absolute;
-//   white-space: nowrap;
-//   width: 1px;
-//   // appearance: none;
-//   // width: 18px;
-//   // aspect-ratio: 1;
-//   // border: 1px solid ${({ theme }) => theme.line};
-//   // border-radius: 50%;
-//   // display: grid;
-//   // place-content: center;
-// `;
-
-// const StyledCheckbox = styled.div`
-//   display: inline-block;
-//   width: 18px;
-//   height: 18px;
-//   border: 1px solid ${({ theme }) => theme.line};
-//   border-radius: 50%;
-//   transition: all 150ms;
-// `;
-
-// const CheckBoxLabel = styled.label`
-//   display: inline-block;
-//   position: relative;
-//   vertical-align: middle;
-//   margin: 5px;
-//   cursor: pointer;
-//   background-color: red;
-// `;
-
-// const Div = styled.div`
-//   width: 20px;
-//   height: 20px;
-//   display: grid;
-//   place-items: center;
-// `;
-
-const TodoTxt = styled.p`
+const TodoText = styled.p`
   flex-grow: 2;
-  max-width: 400px;
+  max-width: 405px;
   line-height: 1.5;
   overflow-wrap: anywhere;
   text-decoration: ${(props) => (props.isCompleted ? "line-through" : "none")};
 `;
 
-const BtnContainer = styled.div`
+const TodoButtons = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
   gap: 10px;
 `;
 
-const TestDiv = styled.div`
+const TodoBtnContainer = styled.div`
   display: grid;
   place-items: center;
 `;
@@ -95,7 +45,7 @@ const TestDiv = styled.div`
 function Todo({ value, id, completed }) {
   const [displayBtn, setDisplayBtn] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(completed);
+  const [checked, setChecked] = useState(completed);
 
   const showModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -111,17 +61,12 @@ function Todo({ value, id, completed }) {
     setDisplayBtn(false);
   };
 
-  const handleChange = () => {
-    setIsCompleted(!isCompleted);
-    handleUpdate();
-  };
-
   const handleUpdate = async () => {
     console.log("change");
     const todoDocRef = doc(db, "todos", id);
     try {
       await updateDoc(todoDocRef, {
-        completed: isCompleted,
+        completed: checked,
       });
     } catch (err) {
       alert(err);
@@ -142,35 +87,22 @@ function Todo({ value, id, completed }) {
     <>
       <TodoItem
         onMouseEnter={(e) => showBtn(e)}
-        onMouseLeave={(e) => hideBtn(e)}
-      >
-        <label>
-          <CheckBox checked={isCompleted} onChange={handleChange} />
-        </label>
-        {/* <CheckBoxContainer>
-          <HiddenCheckBox
-            type="checkbox"
-            id={`checkbox-${id}`}
-            checked={isCompleted}
-            onChange={handleChange}
-          />
-          <CheckBoxLabel
-            htmlFor={`checkbox-${id}`}
-            onClick={() => setIsCompleted(!isCompleted)}
-          >
-            l
-          </CheckBoxLabel>
-          <StyledCheckbox checked={isCompleted} />
-        </CheckBoxContainer> */}
-        <TodoTxt isCompleted={isCompleted}>{value}</TodoTxt>
-        <BtnContainer>
-          <TestDiv onClick={showModal}>
+        onMouseLeave={(e) => hideBtn(e)}>
+        <Checkbox
+          id={id}
+          checked={checked}
+          setChecked={setChecked}
+          handleUpdate={handleUpdate}
+        />
+        <TodoText isCompleted={checked}>{value}</TodoText>
+        <TodoButtons>
+          <TodoBtnContainer onClick={showModal}>
             {displayBtn && <Button btnType="Edit" />}
-          </TestDiv>
-          <TestDiv onClick={handleDelete}>
+          </TodoBtnContainer>
+          <TodoBtnContainer onClick={handleDelete}>
             {displayBtn && <Button btnType="Remove" />}
-          </TestDiv>
-        </BtnContainer>
+          </TodoBtnContainer>
+        </TodoButtons>
       </TodoItem>
       {isModalOpen && (
         <EditTodo showModal={showModal} editValue={value} id={id} />
